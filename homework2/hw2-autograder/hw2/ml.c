ea24f9e6b8b6769bbf1da3c473d913c3e451cc8f
Timothy A. Zhang <taz29@ls.cs.rutgers.edu>
2020-09-26 13:53:30 -0400

    ml2

--- homework2/hw2-autograder/hw2/ml.c
+++ homework2/hw2-autograder/hw2/ml.c
@@ -17,36 +17,61 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+#define PRICE_COUNT 1
+
 // all methods declarations
 double** multiplyMatrix(double **matA, double **matB, int r1, int c1, int r2, int c2);
 double** transposeMatrix(double** mat, int row, int col);
 double** inverseMatrix(double **matA, int dimension);
 void printMatrix(double **matrix, int row, int col);
+void clearMatrix(double **matrix, int row);
 
 // main method starts here
 int main(int argc, char** argv){
 	
 	FILE* fp = fopen(argv[1], "r");
 
-	int rows;
-	int cols;
+	int Xrows;
+	int Xcols;
 
-	fscanf(fp, "%d\n", &cols);
-	cols++;
-	fscanf(fp, "%d\n", &rows);
+	fscanf(fp, "%d\n", &Xcols);
+	fscanf(fp, "%d\n", &Xrows);
 	
-	double** chart = malloc(rows*sizeof(double*));
-	for(int i = 0; i < rows; i++){
-		chart[i] = malloc(cols*sizeof(double));
+	double** mat = malloc(Xrows*sizeof(double*));
+	for(int i = 0; i < Xrows; i++){
+		mat[i] = malloc(Xcols*sizeof(double));
 	}
 
-	for(int r = 0; r < rows; r++){
-		for(int c = 0; c < cols; c++){
-			fscanf(fp, "%le, ", &chart[r][c]);
+	double** Y = malloc(Xrows*sizeof(double*));
+	for(int i = 0; i < Xrows; i++){
+		Y[i] = malloc(sizeof(double));
+	}
+	int Yrows = Xrows;
+	//int Ycols = PRICE_COUNT;
+
+	for(int r = 0; r < Xrows; r++){
+		for(int c = 0; c < Xcols + 1; c++){
+			if(c<Xcols){
+				fscanf(fp, "%le, ", &mat[r][c]);
+			}
+			else{
+				fscanf(fp, "%le, ", &Y[r][0]);
+			}
 		}
 		fscanf(fp, "\n");	
 	}
 
+	double** matTrans = transposeMatrix(mat, Xrows, Xcols);
+	
+	int XTrows = Xcols;
+	int XTcols = Xrows;	
+
+	printMatrix(mat, Xrows, Xcols);
+	printMatrix(matTrans, XTrows, XTcols);
+
+	clearMatrix(matTrans, XTrows);
+	clearMatrix(mat, Xrows);
+	clearMatrix(Y, Yrows);
 	return 0;
 }
 
@@ -110,12 +135,20 @@ double** inverseMatrix(double **matA, int dimension)
 	return matI;
 }
 
-void printMatrix(double **matrix, int row, int col)
-	for(int r = 0; r < rows; r++){
-		for(int c = 0; c < cols; c++){
-		       	printf("%le ", chart[r][c];
+void printMatrix(double **matrix, int row, int col){
+	printf("\n");
+	for(int r = 0; r < row; r++){
+		for(int c = 0; c < col; c++){
+		       	printf("%le ", matrix[r][c]);
 		}
 		printf("\n");
         }
+	printf("\n");
+}
 
+void clearMatrix(double **matrix, int row){
+	for(int r = 0; r < row; r++){
+		free(matrix[r]);
+	}
+	free(matrix);
 }
