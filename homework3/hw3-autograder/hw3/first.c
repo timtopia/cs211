d0e6df87b003c64a09a5cec140be563a4430beef
Timothy A. Zhang <taz29@ls.cs.rutgers.edu>
2020-10-07 23:54:08 -0400

    Files

--- homework3/hw3-autograder/hw3/first.c
+++ homework3/hw3-autograder/hw3/first.c
@@ -1,5 +1,31 @@
 #include <stdio.h>
+#include <string.h>
+#include <math.h>
 
 int main(int argc, char* argv[])
 {
+	FILE* fp = fopen(argv[1], "r");
+
+	unsigned short Number;
+
+	fscanf(fp, "%hu\n", &Number);
+
+	int a, b;
+
+	char[5] word;
+
+	while(fscanf(fp, "%s\t%d\t%d\m", &word, &a, &b) == 3){
+		if(strcmp(word, "comp") == 0){
+			int position = ;
+
+		}
+		else if(strcmp(word, "get") == 0){
+			int position = 2^a;
+		}
+		else if(strcmp(word, "set") == 0){
+		}
+		else{
+		}
+	}
+
 }
