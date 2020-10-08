7dc06413e09fc0ef4d364a4f0d960514a28cfa50
Timothy A. Zhang <taz29@ls.cs.rutgers.edu>
2020-10-08 01:04:45 -0400

    First complete

--- homework3/hw3-autograder/hw3/first.c
+++ homework3/hw3-autograder/hw3/first.c
@@ -12,20 +12,55 @@ int main(int argc, char* argv[])
 
 	int a, b;
 
-	char[5] word;
+	char word[5];
 
-	while(fscanf(fp, "%s\t%d\t%d\m", &word, &a, &b) == 3){
+	while(fscanf(fp, "%s\t%d\t%d\n", &word[0], &a, &b) == 3){
 		if(strcmp(word, "comp") == 0){
-			int position = ;
-
+			int x = 1;
+			x = x << a;
+			int or = Number|x;
+			if(or == Number){
+				Number = Number - x;
+				printf("%hu\n", Number);	
+			}
+			else{
+				printf("%hu\n", or);
+				Number = or;
+			}	
 		}
 		else if(strcmp(word, "get") == 0){
-			int position = 2^a;
+			int temp = Number;
+			Number = Number >> a;
+			if(Number % 2 == 0){
+				printf("0\n");
+			}
+			else if(Number % 2 == 1){
+				printf("1\n");
+			}
+			Number = temp;
 		}
 		else if(strcmp(word, "set") == 0){
+			if(b == 0){
+				int x = 1;
+				x = x << (a-1);
+				if((Number|x) == Number){
+					Number = Number - x;
+				}
+				printf("%hu\n", Number);
+			}
+			else{
+				int x = 1;
+				x = x << a;
+				if((Number|x) == Number){
+					printf("%hu\n", Number);
+				}
+				else{
+					printf("%hu\n", Number + x);
+				}
+			}
 		}
 		else{
+		
 		}
 	}
-
 }
