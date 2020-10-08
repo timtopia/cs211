ad845c2cc6618bb5164a8b36c3566922cf89e8ff
Timothy A. Zhang <taz29@ls.cs.rutgers.edu>
2020-10-08 12:57:49 -0400

    Third

--- homework3/hw3-autograder/hw3/third.c
+++ homework3/hw3-autograder/hw3/third.c
@@ -1,5 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+int get(int number, int a);
 
 int main(int argc, char* argv[])
 {
+	unsigned short num = atoi(argv[1]);
+	
+	for(int end = 0, beginning = 15; end < beginning; end++, beginning--){
+		if(get(num, end) != get(num, beginning)){
+			printf("Is-Palindrome\n");
+			return 0;
+		}
+	}
+	printf("Not-Palindrome\n");
+}
+
+int get(int number, int a){
+	number = number >> a;
+	if(number % 2 == 0){
+		return 0;
+	}
+	else{
+		return 1;
+	}
 }
