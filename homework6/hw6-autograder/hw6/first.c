9e7fb5e5d8d5024c945fd24da450871dbc5eee72
Timothy A. Zhang <taz29@ls.cs.rutgers.edu>
2020-11-20 23:17:11 -0500

    first input

--- homework6/hw6-autograder/hw6/first.c
+++ homework6/hw6-autograder/hw6/first.c
@@ -1,6 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <stdbool.h>
+#include <math.h>
+#include <string.h>
+
+int power_of_2(int n);
+
+int associativityComp(char* str);
 
 int main(int argc, char* argv[])
 {
+	if(argc != 6){
+		printf("error\n");
+		return 1;
+	}
+	if(isdigit(argv[1][0]) == 0){
+		printf("error\n");
+		return 1;
+	}
+	if(power_of_2(atoi(argv[1])) == false){
+		printf("error\n");
+		return 1;
+	}
+	
+}
+
+int power_of_2(int n)
+{
+	if(n <= 0){
+		return false;
+	}
+	return (ceil(log2(n)) == floor(log2(n)));
 }
