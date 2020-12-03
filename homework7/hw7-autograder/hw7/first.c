1a9d26c191d50991356fe0f01638977e3176a821
Timothy A. Zhang <taz29@ls.cs.rutgers.edu>
2020-12-02 23:31:07 -0500

    hw7

--- homework7/hw7-autograder/hw7/first.c
+++ homework7/hw7-autograder/hw7/first.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int main(int argc, char* argv[])
 {
+	FILE* fp = fopen(argv[1], "r");
+	int variables;
+	fscanf(fp, "INPUTVAR %d ", &variables);
+	int rows = pow(2, variables);
+	int chart[rows][variables + 1];
+
 }
